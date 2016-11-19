2d05a3a1c7507421a41d4bab4d3a4a81b9f4cdec
pkreyenhop <Peter.Kreyenhop@gmail.com>
2016-11-19 23:00:31 +1300

    Added tests for Vulpex

---
+++ src/test/java/petersoft/com/VulpexTest.java
@@ -0,0 +1,56 @@
+package petersoft.com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by peterkreyenhop on 19/11/16.
+ */
+class VulpexTest {
+    @Test
+    void getPokeDexNumber() {
+
+    }
+
+    @Test
+    void setPokeDexNumber() {
+
+    }
+
+    @Test
+    void getHealth() {
+
+    }
+
+    @Test
+    void setHealth() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void setName() {
+
+    }
+
+    @Test
+    void getCp() {
+
+    }
+
+    @Test
+    void setCp() {
+
+    }
+
+    @Test
+    void compareTo() {
+
+    }
+
+}
